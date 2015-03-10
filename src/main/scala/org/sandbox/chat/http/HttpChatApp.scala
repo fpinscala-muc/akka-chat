086665d7a719b9d76d919e6d07f646663835c81e
Achim Botz <achim.botz@teradata.com>
2015-03-10 07:39:50 +0100

    first working http server

---
+++ src/main/scala/org/sandbox/chat/http/HttpChatApp.scala
@@ -0,0 +1,43 @@
+package org.sandbox.chat.http
+
+import akka.http.Http
+import akka.stream.ActorFlowMaterializer
+import akka.actor.ActorSystem
+import akka.http.model.HttpRequest
+import akka.http.model.HttpResponse
+import akka.http.model.Uri
+import akka.http.model.HttpEntity
+import akka.http.model.MediaTypes
+
+object HttpChatApp extends App {
+
+  implicit val system = ActorSystem("chat-http")
+  implicit val materializer = ActorFlowMaterializer()
+
+  val serverSource = Http(system).bind(interface = "localhost", port = 8080)
+  serverSource.runForeach { connection => // foreach materializes the source
+    println("Accepted new connection from " + connection.remoteAddress)
+    // ... and then actually handle the connection
+  }
+
+  import akka.http.model.HttpMethods._
+  import akka.stream.scaladsl.{ Flow, Sink }
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+      HttpResponse(
+        entity = HttpEntity(MediaTypes.`text/html`,
+          "<html><body>Hello world!</body></html>"))
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) => HttpResponse(entity = "PONG!")
+    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) => sys.error("BOOM!")
+    case HttpRequest(GET, Uri.Path("/stop"), _, _, _) =>
+      system.shutdown()
+      HttpResponse(entity = "shutting down http server ...")
+    case _: HttpRequest => HttpResponse(404, entity = "Unknown resource!")
+  }
+  val bindingFuture = serverSource.to(Sink.foreach { connection =>
+    println("Accepted new connection from " + connection.remoteAddress)
+    connection handleWithSyncHandler requestHandler
+    // this is equivalent to
+    // connection handleWith { Flow[HttpRequest] map requestHandler }
+  }).run()
+}
