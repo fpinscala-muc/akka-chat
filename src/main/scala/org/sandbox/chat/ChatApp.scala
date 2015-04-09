61993b56a671c780df316cb2ed19f4baebcf2b5e
abo64 <abo64@gmx.de>
2015-04-09 08:09:17 +0200

    separated cluster config

--- src/main/scala/org/sandbox/chat/ChatApp.scala
+++ src/main/scala/org/sandbox/chat/ChatApp.scala
@@ -2,22 +2,18 @@ package org.sandbox.chat
 
 import akka.actor.ActorSystem
 import akka.actor.Props
-import akka.cluster.Cluster
 
 object ChatApp extends App {
 
-  val system = ActorSystem("akka-chat")
-  val settings = Settings(system)
+  val system = ActorSystem("chat-app")
 
-    system.log.debug("Waiting to become a cluster member ...")
-    Cluster(system).registerOnMemberUp {
-//      Sharding(system).start()
-      system.actorOf(Reaper.props, Reaper.Name)
-      system.log.info("Akka-Chat Cluster up and running")
+  val server =
+    system.actorOf(ChatServer.props(true), "ChuckNorris")
+
+  val Seq(client1, client2, client3) =
+    (1 to 3) map { i =>
+      system.actorOf(ChatClient.props(s"client$i", server), s"client$i")
     }
 
-  system.log.info(s"ChatApp with ActorSystem ${system.name} started")
-  system.registerOnTermination(system.log.info(s"ActorSystem ${system.name} shutting down ..."))
-
-  system.awaitTermination
+  system.shutdown
 }
